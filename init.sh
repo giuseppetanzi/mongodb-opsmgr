2b638b0b93da0b01cd4be5e87b78a79a91f13d50
Giuseppe Tanzi <gtanzi@Giuseppes-MBP.station>
2017-11-21 19:49:49 +0100

    Firat commit

---
+++ init.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Initiate replica set configuration
+echo "Configuring the MongoDB Replica Set"
+mongo --host mongod-0.mongodb-service --eval 'rs.initiate({_id: "MainRepSet", version: 1, members: [ {_id: 0, host: "mongod-0.mongodb-service:27017"}, {_id: 1, host: "mongod-1.mongodb-service:27017"}, {_id: 2, host: "mongod-2.mongodb-service:27017"} ]});'
+
+# Wait a bit until the replica set should have a primary ready
+echo "Waiting for the Replica Set to initialise..."
+sleep 5 
+mongo --host mongod-0.mongodb-service --eval 'rs.status();'
+
+LOGFILE="/opt/mongodb/mms/logs/docker-started-mms.log"
+
+echo Running $@
+
+nohup "$@" > $LOGFILE 2>&1 &
+echo after
+sleep 3
+tail -F $LOGFILE & wait ${!}
+
