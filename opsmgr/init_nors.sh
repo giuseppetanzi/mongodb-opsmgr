1c09f657dca1d97535b01cec16ed6b13ae06d7d6
Giuseppe Tanzi <gtanzi@Giuseppes-MacBook-Pro.local>
2017-11-24 11:53:37 +0100

    .

--- opsmgr/init_nors.sh
+++ opsmgr/init_nors.sh
@@ -9,9 +9,8 @@ LOGFILE="/opt/mongodb/mms/logs/docker-started-mms.log"
 sigterm_handler() {
   echo "AGENT-WRAPPER: Stopping mms service"
   /opt/mongodb/mms/bin/mongodb-mms stop
-  killall -w -SIGTERM tail || true
   kill -KILL 1 || true
-  echo "AGENT-WRAPPER: Finished hanging up mongos/mongod/agent processes"
+  echo "AGENT-WRAPPER: Finished stopping mms processes"
   exit 143
 }
 
